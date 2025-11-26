865b397d0714825a8f67a45c3ddf7c31992e1688
LuisMinuttiTR <luis.minutti@thomsonreuters.com>
2025-11-26 14:49:40 -0600

    Adding an array of animal names that get printed on the screen

---
+++ src/Greeting.jsx
@@ -0,0 +1,14 @@
+
+function Salutation(){
+    return <h1>This is a Salutation</h1>
+}
+
+function FoodOne(){
+    return <h1>Pizza</h1>
+}
+
+function FoodTwo(){
+    return <h1>Burger</h1>
+}
+
+export { Salutation, FoodOne, FoodTwo };
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Salutation, FoodOne, FoodTwo } from './Greeting.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Salutation />
+    <FoodOne />
+    <FoodTwo />
     <App />
   </StrictMode>,
 )
